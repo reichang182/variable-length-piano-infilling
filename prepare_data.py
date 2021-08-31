6576760c0d9f55082b0f1c375f7bf97cfdffb124
reichang182 <88315537+reichang182@users.noreply.github.com>
2021-08-31 13:15:05 +0800

    Update prepare_data.py
    Fix bug in prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -11,6 +11,8 @@ import random
 import copy
 import statistics
 
+GroupEvent = collections.namedtuple('GroupEvent', ['Tempo', 'Bar', 'Position', 'Pitch', 'Duration', 'Velocity'])
+tempo_quantize_step = 4
 
 def extract_events(input_path):
     note_items, tempo_items = utils.read_items(input_path)
@@ -474,9 +476,6 @@ if __name__ == '__main__':
     parser.add_argument('--midi-folder', type=str, default='datasets/midi/midi_synchronized', help="Folder containing the midi files.")
     parser.add_argument('--save-folder', type=str, default='./', help="Folder to save worded_data and dictionary.")
     args = parser.parse_args()
-
-    GroupEvent = collections.namedtuple('GroupEvent', ['Tempo', 'Bar', 'Position', 'Pitch', 'Duration', 'Velocity'])
-    tempo_quantize_step = 4
     
     # for loading training data
     convert_midis_to_worded_data(args.midi_folder, args.save_folder)
